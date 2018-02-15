56ac924dba397fce0c581e40e7e1813f5b771e88
Néstor "Hackerman <alvarado.nestor@yandex.com>
2018-02-15 08:22:14 -0600

    ADD: index.php registro.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Bienvenida</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+
+<body>
+	<h2>Acerca de nosotros</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, se
+	</p>
+	<br/>
+	
+	<h2>Mision</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, se
+	</p>
+	<br/>
+	
+	<h2>Vision</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, se
+	</p>
+	<br/>
+	
+	<h2>Valores</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, se
+	</p>
+	<br/>
+</body>
+
+</html>
---
+++ registro.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Registro de usuarios</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+
+<body>
+	<h2>Registro de usuarios</h2>
+	<p>
+		Para registrar un usuario completa el formulario con los datos que se solicitan y al finalizar presiona el boton de "enviar"
+	</p>
+	
+	<form action="#" method="post">
+		Nombre:<br/>
+		<input type="text" name="nombre"><br/>
+		Apellido paterno:<br/>
+		<input type="text" name="apellidoPaterno"><br/>
+		Apellido materno:<br/>
+		<input type="text" name="apellidoMaterno"><br/>
+		Telefono:<br/>
+		<input type="tel" name="telefono"><br/>
+		Correo:<br/>
+		<input type="email" name="correo"><br/>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+
+</html>
