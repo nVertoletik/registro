9b6faab361a452216390e23cedae60bc2ec60707
Néstor "Hackerman <alvarado.nestor@yandex.com>
2018-03-01 08:51:56 -0600

    EDIT: estructura

---
+++ estructura.php
@@ -0,0 +1,26 @@
+<?php
+	$cifra1 =5;
+	$cifra2 =8;
+	
+	function suma_numeros($var1, $var2){
+		$suma = $var1 + $var2;
+		return $suma;
+	}
+	
+	$monto_total = suma_numeros($cifra1, $cifra2);
+	echo "La suma total es: ".$monto_total;
+?>
+
+/*
+$con = pg_connect("host port dbname user password");
+$result = pg_query($con, "select * from tabla");
+$resp = pg_fetch_array($result);
+pg_close($con);
+
+manejo con errores
+$con = pg_connect("host port dbname user password") or die(pg_last_error());
+
+inserciones o actualizaciones
+$query = "insert into tables values('$_POST[param1]', '$_POST[param2]')";
+$result = pg_query($query);
+*/
