64dcaf5fc89650d2001e4d545e9275f2a1dca37d
Néstor "Hackerman <alvarado.nestor@yandex.com>
2018-03-24 19:27:24 -0600

    Add: Filtros y validaciones

--- alta_registro.php
+++ alta_registro.php
@@ -13,11 +13,28 @@ $apellidoPaterno = $_POST['apellidoPaterno'];
 $apellidoMaterno = $_POST['apellidoMaterno'];
 $telefono = $_POST['telefono'];
 $correo = $_POST['correo'];
+
+/*if(empty($nombre)){
+	$nombre = filter_var($nombre, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+}
+empty($apellidoPaterno);
+empty($apellidoMaterno);
+empty($telefono);
+
+if(!empty($correo)){
+	$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);;
+}*/
+
+echo "Nombre: ".$nombre."<br/>";
+echo "Apellido Paterno: ".$apellidoPaterno."<br/>";
+echo "Apellido Materno: ".$apellidoMaterno."<br/>";
+echo "Telefono: ".$telefono."<br/>";
+echo "Correo: ".$correo."<br/>";
 if(!empty($nombre)||!empty($correo)||!empty($apellidoMaterno)||!empty($apellidoPaterno)||!empty($telefono)){
 	$comentario = "insert into usuarios (nombre,apaterno,amaterno,telefono, correo) values('$nombre','$apellidoPaterno','$apellidoMaterno','$telefono','$correo')";
 	$guarda_comentario = consulta($comentario);
 	if($guarda_comentario == false){
-		echo "Gracias por dejar tu comentario, en caso de ser necesario nuestros colaboradores se pondrán en contacto contigo";
+		echo "Datos ingresados de manera correcta";
 	}
 	else{
 		echo "Hubo un error al intentar guardar tu comentario, intenta más tarde";
@@ -27,5 +44,9 @@ else{
 	echo "Los valores ingresados no son válidos";
 }
 ?>
+
+<br/>
+<a href="registro.php">Regresar al formulario</a>
+
 </body>
 </html>
